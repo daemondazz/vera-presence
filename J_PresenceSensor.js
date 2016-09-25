e0540e8a95aca010b40f55024768bade8fc44d21
GeekGoneOld <laidlaw1546@gmail.com>
2016-09-24 21:44:27 -0400

    Add notifications and tidy up
    1) add notifications
    2) fix deviceType name
    3) change icons
    4) delete unused services and variables
    5) tidy up files re newline, tabs etc.
    6) delete deprecated code (e.g. eventList)

--- J_PresenceSensor.js
+++ J_PresenceSensor.js
@@ -3,10 +3,17 @@ var presencesensor_svs = 'urn:afoyi-com:serviceId:PresenceSensor1';
 function presencesensor_settings(deviceID) {
     var address  = get_device_state(deviceID,  presencesensor_svs, 'Address');
     var devicetype  = get_device_state(deviceID,  presencesensor_svs, 'DeviceType');
-    var html =  '<table>' +
-        '<tr><td>Address </td><td><input  type="text" id="presence_address" size=10 value="' +  address + '" onchange="address_set(' + deviceID + ', this.value);"></td></tr>' +
-        '<tr><td>Device Type </td><td><input  type="text" id="presence_devicetype" size=10 value="' +  devicetype + '" onchange="devicetype_set(' + deviceID + ', this.value);"></td></tr>' +
-        '</table>';
+    if (devicetype == "bluetooth") {
+        var html =  '<table>' +
+            '<tr><td>Address </td><td><input  type="text" id="presence_address" size=20 value="' +  address + '" onchange="address_set(' + deviceID + ', this.value);"></td></tr>' +
+            '<tr><td>Device Type </td><td><select onchange="devicetype_set(' + deviceID + ', this.value);"><option value="bluetooth" selected>Bluetooth (phone, tablet etc.)</option><option value="ibeacon">iBeacon</option></select></td></tr>' +
+            '</table>';
+    } else {
+        var html =  '<table>' +
+            '<tr><td>Adress </td><td><input  type="text" id="presence_address" size=20 value="' +  address + '" onchange="address_set(' + deviceID + ', this.value);"></td></tr>' +
+            '<tr><td>Device Type </td><td><select onchange="devicetype_set(' + deviceID + ', this.value);"><option value="bluetooth">Bluetooth (phone, tablet etc.)</option><option value="ibeacon" selected>iBeacon</option></select></td></tr>' +
+            '</table>';
+    }
     set_panel_html(html);
 }
 
